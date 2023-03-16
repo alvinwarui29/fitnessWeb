44eb89e3dcb52a23313cbdde74d5670e2c9db1b4
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-16 09:44:35 +0300

    make navbar responsive and add toggle button functionality

--- src/pages/navbar/Navbar.jsx
+++ src/pages/navbar/Navbar.jsx
@@ -1,29 +1,35 @@
 import "./navbar.css";
+import {useState} from 'react';
 import React from 'react'
 import {Link,NavLink} from "react-router-dom";
 import Logo from "./../../images/logo.png"
 import  {links} from "./../../data";
 import {FaBars} from 'react-icons/fa';
+import {AiOutlineCloseCircle} from 'react-icons/ai';
 const Navbar = () => {
+  const [isNavShowing,setIsNavShowing] = useState(false);
   return (
     <nav>
     <div className="container nav__container">
         <Link to="/" className="logo">
           <img src={Logo} alt="Logo Image"/>
         </Link>
-        <ul className="nav__links">
+        <ul className={`nav__links ${isNavShowing ? 'show__nav' : 'hide__nav' }`}>
             {
               links.map(({name,path},index)=>{
                 return(
                   <li>
-                  <NavLink to={path}>{name}</NavLink>
+                  <NavLink to={path} className={({isActive})=> isActive ? 'active-nav' : ""} >{name}</NavLink>
                 </li>
                 )
               })
             }
         </ul>
-        <button className="nav__toogle-btn">
-          <FaBars/>
+        <button className="nav__toogle-btn" onClick={()=>
+        setIsNavShowing(!isNavShowing)}>
+          {
+            isNavShowing ? <AiOutlineCloseCircle/> : <FaBars/>
+          }
         </button>
 
     </div>
